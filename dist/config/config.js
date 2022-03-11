8619d59a415a5c41979243d9acd66e82e0a2332d
Diego Bustamante <Diego Bustamante>
2022-03-10 21:14:39 -0300

    Se añade variable de entorno SERVER_PORT

--- dist/config/config.js
+++ dist/config/config.js
@@ -12,7 +12,8 @@ var configurations = {
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME
+  DB_NAME: process.env.DB_NAME,
+  SERVER_PORT: process.env.SERVER_PORT
 };
 var _default = configurations;
 exports["default"] = _default;
