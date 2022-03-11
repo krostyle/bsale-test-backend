eaab7d4fdc77c469dc5bda523dd8a275c311c5c7
Diego Bustamante <Diego Bustamante>
2022-03-10 21:14:17 -0300

    Se añade variable de entorno SERVER_PORT

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,7 @@ const configurations = {
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_NAME: process.env.DB_NAME,
+    SERVER_PORT: process.env.SERVER_PORT,
 }
 
 
