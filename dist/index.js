dc751b6299165cf78e8947a132356fb64c18d1ae
Diego Bustamante <Diego Bustamante>
2022-03-10 22:26:29 -0300

    se cambia puerto

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,13 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
+var _config = _interopRequireDefault(require("./config/config"));
+
 var _server = _interopRequireDefault(require("./server/server"));
 
 console.log(_server["default"].get('port'));
 
-_server["default"].listen(_server["default"].get('port'), function () {
+_server["default"].listen('port', function () {
   console.log("Server on port ".concat(_server["default"].get("port")));
   console.log("http://localhost:".concat(_server["default"].get("port")));
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import configurations from './config/config';
 import app from './server/server';
 
 console.log(app.get('port'));
 
-app.listen(app.get('port'), () => {
+app.listen('port', () => {
     console.log(`Server on port ${app.get("port")}`);
     console.log(`http://localhost:${app.get("port")}`);
 });
